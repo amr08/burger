552fb4926088c1ac7a7cfbc9150a93f27a3d3e8a
Andrea R <amrdev08@gmail.com>
2016-10-25 03:27:48 -0400

    fixed port

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-/*
-Here is where you set up your server file.
-express middleware.
-*/
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+	var express = require('express');
+	var bodyParser = require('body-parser');
+	var methodOverride = require('method-override');
 
-var app = express();
+	var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'));
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+	app.use(express.static(process.cwd() + '/public'));
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}));
 
-var port = process.env.PORT || 7000;
-app.listen(port);
+	app.use(methodOverride('_method'));
+	var exphbs = require('express-handlebars');
+	app.engine('handlebars', exphbs({
+		defaultLayout: 'main'
+	}));
+	app.set('view engine', 'handlebars');
+
+	var routes = require('./controllers/burgers_controller.js');
+	app.use('/', routes);
+
+	var PORT = process.env.PORT || 7000;
+	app.listen(PORT);
 
 
 
